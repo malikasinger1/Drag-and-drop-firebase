c5c7de17ac0e41ee64eb03e28bda2bf5639cee23
= <=>
2016-05-18 20:59:17 +0500

    bjhmh

--- javascripts/loginAdmin.js
+++ javascripts/loginAdmin.js
@@ -1,82 +1,164 @@
 /*
  * Created by Welcome on 2/5/2016.
  */
-
 angular.module("App")
-
-
     .controller("loginCtrl", function ($scope, $firebaseArray, $firebaseObject, $q) {
-        var refAdd = new Firebase("https://draganddropsdk.firebaseio.com/add")
-
+        var refAdd = new Firebase("https://teamofteam.firebaseio.com/projects/projectId1234/Doing")
 
         $scope.addFemale = function (user) {
-            refAdd.push(user)
+            refAdd.orderByChild("serial").limitToLast(1).on("value", function (snapshot) {
+                snapshot.forEach(function (data) {
+                    console.log(data.val().serial);
+                    user.serial = data.val().serial + 1;
+                });
+            });
+
+            refAdd.push(user);
         }
-        refAdd.on("value", function (snapshot) {
-            $scope.add = snapshot.val()
-            //    console.log($scope.add)  
-            setTimeout(function () {
-                $scope.$apply()
-            }, 100)
+
+
+        refAdd.orderByChild("serial").on('child_changed', function (childSnapshot, prevChildKey) {
+
+            console.log('child is changed');
         });
 
+        var cm = this;
+        $scope.data = {};
+        refAdd.orderByChild("serial").on('child_added', function (childSnapshot, prevChildKey) {
+            //    console.log("C:",childSnapshot.val());
+            //   console.log("C:",childSnapshot.key());
+            //    data. push(childSnapshot.val());
 
-        var refDoing = new Firebase("https://draganddropsdk.firebaseio.com/diong")
-        // $scope.addMale = function (user) {
-        //     refDoing.push(user)
-        // }
-        refDoing.on("value", function (snapshot) {
-            $scope.diong = snapshot.val()
-            //    console.log($scope.diong)
-            setTimeout(function () {
-                $scope.$apply()
-            }, 1000)
-        })
+            $scope.data[childSnapshot.key()] = childSnapshot.val();
+
+            $scope.serial = childSnapshot.val();
 
 
-        var refReview = new Firebase("https://draganddropsdk.firebaseio.com/review")
+            $scope.$apply();
 
-        refReview.on("value", function (snapshot) {
-            $scope.review = snapshot.val()
-            setTimeout(function () {
-                $scope.$apply()
-            }, 100)
-        })
 
-        var refDone = new Firebase("https://draganddropsdk.firebaseio.com/done")
-
-        refDone.on("value", function (snapshot) {
-            $scope.done = snapshot.val()
-            setTimeout(function () {
-                $scope.$apply()
-            }, 100)
-        })
-
+            // console.log($scope.data);
+        });
 
         $scope.dropSuccessHandler = function ($event, index, array) {
-            var k = Object.keys(array).splice(index, 1)
-            console.log(k)
-            k = k[0];
-            refDone.child(k).remove();
-            refReview.child(k).remove();
-            refDoing.child(k).remove();
-            refAdd.child(k).remove();
+
+            $scope.currentIndex = index;
+            $scope.currentId = Object.keys(array).splice(index, 1);
+            // $scope.currentdata   = Object.keys(array).splice(index, 1);
+            var data = array[$scope.currentId];
+            droped(data);
+
+            //var k = Object.keys(array).splice(index, 1);
+            //console.log(index , k);
+            //  $scope.previous  = $scope.add[k];
+            //   $scope.previousIndex =  k ;
+            //    swap($scope.current, $scope. $scope.previous , $scope.previousIndex);
+
+            //var refAdd = new Firebase("https://teamofteam.firebaseio.com/projects/projectId1234/Doing"+'/' + k )
+            //
+            //refAdd.set({
+            //    detail:'aaaa',
+            //    other:'oooo',
+            //    title :'bbbb'
+            //})
+
+
+            //var k = Object.keys(array).splice(index, 1)
+            //console.log(k)
+            //k = k[0];
+            //refDone.child(k).remove();
+            //refReview.child(k).remove();
+            //refDoing.child(k).remove();
+            //refAdd.child(k).remove();
 
 
         };
+        $scope.onDrop = function ($event, index, array) {
+            //
+
+            $scope.previousIndex = index;
+            $scope.previousKey = Object.keys(array).splice(index, 1);
 
 
-        $scope.onDrop = function ($event, $data, array) {
-            refDoing.push($data);
-        };
-        $scope.onDrop1 = function ($event, $data, array) {
-            refAdd.push($data);
-        };
-        $scope.onDrop2 = function ($event, $data, array) {
-            refReview.push($data);
-        };
-        $scope.onDrop3 = function ($event, $data, array) {
-            refDone.push($data);
-        };
+            //var k = Object.keys(array).splice(index, 1);
+            ////console.log(index , k);
+            //$scope.current  = $scope.add[k];
+
+
+            //var refAdd = new Firebase("https://teamofteam.firebaseio.com/projects/projectId1234/Doing"+'/' + k )
+
+            //refAdd.set({
+            //    detail:'aaaa',
+            //    other:'oooo',
+            //    title :'bbbb'
+            //})
+
+            //swap
+
+            //  var refAdd = new Firebase("https://teamofteam.firebaseio.com/projects/projectId1234/Doing"+'/' + k )
+
+            //refAdd.set({
+            //    detail:'aaaa',
+            //    other:'oooo',
+            //    title :'bbbb'
+            //})
+
+
+        }
+        function droped(data) {
+
+            var mean = ($scope.currentIndex - $scope.previousIndex) / 2;
+            var refAdd = new Firebase("https://teamofteam.firebaseio.com/projects/projectId1234/Doing" + '/' + $scope.currentId)
+
+            refAdd.set({
+                detail: data.detail,
+                serial: mean,
+
+            })
+
+        }
+
+
+        function calcSerial(currentIndex, PreviousIndex) {
+
+            console.log(currentIndex);
+            console.log(PreviousIndex);
+
+
+            return (currentIndex + PreviousIndex) / 2;
+
+        }
+
+        function swap(current, previous) {
+
+            console.log(current);
+
+            //var refAdd = new Firebase("https://teamofteam.firebaseio.com/projects/projectId1234/Doing"+'/' + current );
+            //
+            //var obj = {};
+            //obj.push(refAdd);
+            //
+            //refAdd.set(obj);
+
+            //var refAdd = new Firebase("https://teamofteam.firebaseio.com/projects/projectId1234/Doing"+'/' + previous );
+            //refAdd.set(previous);
+
+            //   console.log(current , previous);
+        }
+
+        //$scope.onDrop = function ($event, $data, array) {
+        //    refDoing.push($data);
+        //};
+        //$scope.onDrop1 = function ($event, $data, array) {
+        //    refAdd.push($data);
+        //};
+        //$scope.onDrop2 = function ($event, $data, array) {
+        //    refReview.push($data);
+        //};
+        //$scope.onDrop3 = function ($event, $data, array) {
+        //    refDone.push($data);
+        //};
+
+
     });
 
